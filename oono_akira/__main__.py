2419c1ba78ae0d69334b3c4bf5e423158ea919df
Mut Ze <mutongsoft@gmail.com>
2023-01-24 03:31:39 +0800

    Remove process_config from __main__

--- oono_akira/__main__.py
+++ oono_akira/__main__.py
@@ -1,27 +1,21 @@
-import os
 import sys
 import json
 import asyncio
+
 from oono_akira.oono import OonoAkira
+from oono_akira.type import Configuration
 
 
-def process_config(config: dict):
-    config["server"]["ssl"]["cert"] = os.path.expanduser(config["server"]["ssl"]["cert"])
-    config["server"]["ssl"]["key"] = os.path.expanduser(config["server"]["ssl"]["key"])
-    config["db"]["path"] = os.path.expanduser(config["db"]["path"])
-
-
-async def amain(config: dict):
+async def amain(config: Configuration):
     async with OonoAkira(config) as oono:
         await oono.run()
 
 
 if __name__ == "__main__":
+
     config_path = sys.argv[1]
     with open(config_path) as f:
         config = json.load(f)
-
-    process_config(config)
 
     try:
         asyncio.run(amain(config))
