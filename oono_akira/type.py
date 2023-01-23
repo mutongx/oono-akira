e735e7b90934eb9087a9c64d521353048c27bc52
Mut Ze <mutongsoft@gmail.com>
2023-01-24 03:30:13 +0800

    Add type definition for configuration file

---
+++ oono_akira/type.py
@@ -0,0 +1,27 @@
+from typing import List, Optional, TypedDict
+
+class ServerSslConfiguration(TypedDict):
+    cert: str
+    key: str
+    port: int
+
+class ServerConfiguration(TypedDict):
+    ssl: ServerSslConfiguration
+
+class SqliteDatabaseConfiguration(TypedDict):
+    path: str
+
+class DatabaseConfiguration(TypedDict):
+    sqlite: Optional[SqliteDatabaseConfiguration]
+
+class SlackConfiguration(TypedDict):
+    client_id: str
+    client_secret: str
+    redirect_uri: str
+    token: str
+    permissions: List[str]
+
+class Configuration(TypedDict):
+    server: ServerConfiguration
+    database: DatabaseConfiguration
+    slack: SlackConfiguration
