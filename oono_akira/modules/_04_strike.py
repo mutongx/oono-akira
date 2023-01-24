3ac7496338f428486d9369a70f49c9af17c0ffa4
Mut Ze <mutongsoft@gmail.com>
2023-01-24 21:00:32 +0800

    Make strike work

--- oono_akira/modules/_04_strike.py
+++ oono_akira/modules/_04_strike.py
@@ -1,31 +1,16 @@
-from typing import Optional, Union
-from oono_akira.modules.__base__ import ModuleBase
+from oono_akira.modules import HandlerType, register
 from oono_akira.slack import SlackContext
 
-class Strike(ModuleBase):
 
-    @staticmethod
-    def check_message(context: SlackContext) -> Optional[str]:
-        blocks = context["event"].get("blocks")
-        if not blocks or len(blocks) != 1:
-            return
-        block = blocks[0]
-        elements = block.get("elements")
-        if not elements or len(elements) != 1:
-            return
-        elements = elements[0].get("elements")
-        if not elements or len(elements) != 1:
-            return
-        element = elements[0]
-        if element.get("style", {}).get("strike"):
-            return ""
-
-    async def process(self):
-        event = self._slack_context["event"]
+@register("message")
+def handler(context: SlackContext) -> HandlerType:
+    async def process(context: SlackContext):
+        context["ack"]()
+        event = context["event"]
         user_id = event["user"]
         if user_id == "USLACKBOT":
             return
-        profile = await self._slack_api.users.info({"user": user_id})
+        profile = await context["api"].users.info({"user": user_id})
         text = event["blocks"][0]["elements"][0]["elements"][0]["text"]
         body = {
             "channel": event["channel"],
@@ -33,6 +18,18 @@ class Strike(ModuleBase):
         }
         if "thread_ts" in event:
             body["thread_ts"] = event["thread_ts"]
-        await self._slack_api.chat.postMessage(body)
+        await context["api"].chat.postMessage(body)
 
-MODULE = Strike
+    blocks = context["event"].get("blocks")
+    if not blocks or len(blocks) != 1:
+        return
+    block = blocks[0]
+    elements = block.get("elements")
+    if not elements or len(elements) != 1:
+        return
+    elements = elements[0].get("elements")
+    if not elements or len(elements) != 1:
+        return
+    element = elements[0]
+    if element.get("style", {}).get("strike"):
+        return "", process
