97df5d7fe616c43689048149749d9e34a50579b1
Mut Ze <mutongsoft@gmail.com>
2023-01-24 14:24:18 +0800

    Fix type hint in slack.py

--- oono_akira/slack.py
+++ oono_akira/slack.py
@@ -1,34 +1,40 @@
 from oono_akira.log import log
-from typing import Tuple, Optional
+from typing import Tuple, Optional, TypedDict, Dict, Any
 from aiohttp import ClientSession
+
+
+AnyDict = Dict[Any, Any]
+
 
 class SlackAPI:
 
     _REQ = {
         "oauth.v2.access": {
             "method": "post",
-            "mime": "application/x-www-form-urlencoded"
+            "mime": "application/x-www-form-urlencoded",
         },
         "users.info": {
             "method": "get",
         },
     }
 
-    def __init__(self, session: ClientSession, auth: Optional[dict] = None, path: Tuple[str, ...] = tuple()):
+    def __init__(
+        self,
+        session: ClientSession,
+        token: Optional[str] = None,
+        path: Tuple[str, ...] = tuple(),
+    ):
         self._session = session
-        self._auth = auth
+        self._token = token
         self._path = path
 
     def __getattr__(self, key: str):
-        return SlackAPI(self._session, self._auth, self._path + (key,))
+        return SlackAPI(self._session, self._token, self._path + (key,))
 
-    async def __call__(self, data=None, **kwargs):
+    async def __call__(self, __data: Optional[AnyDict] = None, **kwargs: AnyDict):
         # Prepare request payload
-        payload = {}
-        headers = {}
-        if data is not None:
-            payload.update(data)
-        payload.update(kwargs)
+        payload: AnyDict = dict(**__data if __data else {}, **kwargs)
+        headers: AnyDict = {}
         # Prepare request argument
         api = ".".join(self._path)
         req = self._REQ.get(api, {})
@@ -36,27 +42,24 @@ class SlackAPI:
         body = {}
         if method == "post":
             mime = req.get("mime", "application/json")
-            headers = { "Content-Type": f"{mime}; charset=UTF-8" }
+            headers = {"Content-Type": f"{mime}; charset=UTF-8"}
             if mime == "application/x-www-form-urlencoded":
                 body["data"] = payload
             elif mime == "application/json":
                 body["json"] = payload
         elif method == "get":
             body["params"] = payload
-        if self._auth and self._auth.get("token") is not None:
-            headers["Authorization"] = f"Bearer {self._auth['token']}"
+        if self._token:
+            headers["Authorization"] = f"Bearer {self._token}"
         # Send request
         resp = await self._session.request(
-            method,
-            f"https://slack.com/api/{api}",
-            headers=headers,
-            **body
+            method, f"https://slack.com/api/{api}", headers=headers, **body
         )
         result = await resp.json()
-        if not result.get('ok'):
+        if not result.get("ok"):
             log(f"Error: {result}")
         return result
 
 
-class SlackContext(dict):
+class SlackContext(TypedDict):
     pass
