ad1461c583e1c3ff50e71c22505ef4b3082d5760
Mut Ze <mutongsoft@gmail.com>
2023-01-24 15:57:07 +0800

    Remove __base__

--- oono_akira/modules/__base__.py
+++
@@ -1,6 +0,0 @@
-from oono_akira.slack import SlackAPI, SlackContext
-
-class ModuleBase:
-    def __init__(self, slack_api: SlackAPI, slack_context: SlackContext):
-        self._slack_api = slack_api
-        self._slack_context = slack_context
