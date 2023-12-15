939cf341a32f8f1efd0c6bbddae2a6be3c6fdffb
Mut Ze <mutongsoft@gmail.com>
2023-12-15 23:20:46 +0800

    Reorganize db to use prisma

--- oono_akira/db.py
+++ oono_akira/db.py
@@ -1,168 +1,93 @@
 import json
-import sqlite3
-from contextlib import closing, contextmanager
+from contextlib import asynccontextmanager
 from datetime import datetime
 from typing import Any
+from prisma import Prisma
 
 from oono_akira.config import DatabaseConfiguration
 
 
-class SessionStorage:
-    def __init__(self, db: sqlite3.Connection, key: str) -> None:
-        self._db = db
-        self._key = key
-
-    def __enter__(self):
-        self._cursor = self._db.cursor()
-        self._cursor.execute(
-            "SELECT content FROM session WHERE key=:key", {"key": self._key}
-        )
-        row = self._cursor.fetchone()
-        self._data: Any = json.loads(row[0]) if row and row[0] else {}
-        return self
-
-    @property
-    def data(self) -> Any:
-        return self._data
-
-    def __exit__(self, *_):
-        self._cursor.execute(
-            """
-            INSERT INTO session(key, content, updated_at)
-            VALUES (:key, :content, :updated_at)
-            ON CONFLICT(key)
-            DO UPDATE SET
-                content=:content,
-                updated_at=:updated_at
-        """,
-            {
-                "key": self._key,
-                "content": json.dumps(self._data),
-                "updated_at": datetime.now(),
-            },
-        )
-        self._cursor.close()
-        self._db.commit()
-
-
 class OonoDatabase:
     def __init__(self, conf: DatabaseConfiguration) -> None:
-        if conf["sqlite"]:
-            self._file = conf["sqlite"]["path"]
-            self._db = sqlite3.connect(self._file)
+        if conf["provider"] == "sqlite":
+            self._client = Prisma(datasource={"url": conf["url"]})
         else:
-            raise RuntimeError("database not specified")
+            raise RuntimeError("unknown database provider")
 
-    @staticmethod
-    def _table_exists(cursor: sqlite3.Cursor, table_name: str) -> bool:
-        result = cursor.execute(
-            "SELECT name FROM sqlite_master WHERE type='table' AND name=:table",
-            {"table": table_name},
-        )
-        return result.fetchone() is not None
+    async def __aenter__(self):
+        await self._client.connect()
+        return self
 
-    def initialize(self):
-        with closing(self._db.cursor()) as cur:
-            if not self._table_exists(cur, "workspace"):
-                cur.execute(
-                    """
-                    CREATE TABLE workspace (
-                        id TEXT PRIMARY KEY,
-                        name TEXT NOT NULL,
-                        bot TEXT NOT NULL,
-                        admin TEXT NOT NULL,
-                        token TEXT NOT NULL,
-                        updated_at TEXT NOT NULL
-                    )
-                """
-                )
-            if not self._table_exists(cur, "payload"):
-                cur.execute(
-                    """
-                    CREATE TABLE payload (
-                        type TEXT NOT NULL,
-                        time TEXT NOT NULL,
-                        content TEXT NOT NULL
-                    )
-                """
-                )
-            if not self._table_exists(cur, "session"):
-                cur.execute(
-                    """
-                    CREATE TABLE session (
-                        key TEXT PRIMARY KEY,
-                        content TEXT NOT NULL,
-                        updated_at TEXT NOT NULL
-                    )
-                """
-                )
+    async def __aexit__(self, *_):
+        await self._client.disconnect()
 
-    def record_payload(self, payload_type: str, payload_content: str | Any):
-        if not isinstance(payload_content, str):
-            payload_content = json.dumps(payload_content)
-        with closing(self._db.cursor()) as cur:
-            cur.execute(
-                "INSERT INTO payload VALUES (:type, :time, :content)",
-                {
-                    "type": payload_type,
-                    "time": datetime.now(),
-                    "content": payload_content,
+    async def record_payload(self, source: str, content: str | Any):
+        if not isinstance(content, str):
+            content = json.dumps(content)
+        return await self._client.payload.create({"source": source, "content": content, "createdAt": datetime.now()})
+
+    async def setup_workspace(self, id: str, name: str, bot_id: str, admin_id: str, token: str, hook_url: str):
+        return await self._client.workspace.upsert(
+            {"id": id},
+            {
+                "create": {
+                    "id": id,
+                    "name": name,
+                    "botId": bot_id,
+                    "adminId": admin_id,
+                    "token": token,
+                    "hookUrl": hook_url,
+                    "createdAt": datetime.now(),
+                    "updatedAt": datetime.now(),
                 },
-            )
-            self._db.commit()
-
-    def add_workspace(
-        self,
-        workspace_id: str,
-        workspace_name: str,
-        bot_id: str,
-        admin_id: str,
-        access_token: str,
-    ):
-        with closing(self._db.cursor()) as cur:
-            cur.execute(
-                """
-                INSERT INTO workspace(id, name, bot, admin, token, updated_at)
-                VALUES (:id, :name, :bot, :admin, :token, :updated_at)
-                ON CONFLICT(id)
-                DO UPDATE SET
-                    name=:name,
-                    bot=:bot,
-                    token=:token,
-                    updated_at=:updated_at
-            """,
-                {
-                    "id": workspace_id,
-                    "name": workspace_name,
-                    "bot": bot_id,
-                    "admin": admin_id,
-                    "token": access_token,
-                    "updated_at": datetime.now(),
+                "update": {
+                    "name": name,
+                    "botId": bot_id,
+                    "adminId": admin_id,
+                    "token": token,
+                    "hookUrl": hook_url,
+                    "updatedAt": datetime.now(),
                 },
-            )
-            self._db.commit()
-
-    def get_workspace_info(self, workspace_id: str):
-        with closing(self._db.cursor()) as cur:
-            rows = cur.execute(
-                "SELECT name, bot, admin, token FROM workspace WHERE id=:id",
-                {"id": workspace_id},
-            )
-            row = rows.fetchone()
-            if row is None:
-                return
-            name, bot, admin, token = row
-        return {
-            "workspace_name": name,
-            "bot_id": bot,
-            "admin_id": admin,
-            "workspace_token": token,
-        }
-
-    @contextmanager
-    def get_session(self, **kwargs: str):
-        session_key = ",".join(
-            f"{key}={value}" for key, value in sorted(kwargs.items())
+            },
         )
-        with SessionStorage(self._db, session_key) as ss:
-            yield ss
+
+    async def get_workspace(self, id: str):
+        return await self._client.workspace.find_unique({"id": id})
+
+    async def lock(self, workspace: str, channel: str, module: str):
+        return await self._client.lock.upsert(
+            {"workspace_channel_module": {"workspace": workspace, "channel": channel, "module": module}},
+            {"create": {"workspace": workspace, "channel": channel, "module": module}, "update": {}},
+        )
+
+    async def unlock(self, workspace: str, channel: str, module: str):
+        return await self._client.lock.delete(
+            {"workspace_channel_module": {"workspace": workspace, "channel": channel, "module": module}}
+        )
+
+    async def get_lock(self, workspace: str, channel: str, module: str):
+        return await self._client.lock.find_unique(
+            {"workspace_channel_module": {"workspace": workspace, "channel": channel, "module": module}}
+        )
+
+    @asynccontextmanager
+    async def get_session(self, **kwargs: str):
+        key = ",".join(f"{key}={value}" for key, value in sorted(kwargs.items()))
+        session = await self._client.session.upsert(
+            {"key": key},
+            {
+                "create": {
+                    "key": key,
+                    "content": "{}",
+                    "createdAt": datetime.now(),
+                    "updatedAt": datetime.now(),
+                },
+                "update": {},
+            },
+        )
+        data = json.loads(session.content)
+        yield data
+        await self._client.session.update(
+            {"content": json.dumps(data), "updatedAt": datetime.now()},
+            {"key": key},
+        )
