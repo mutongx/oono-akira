de11f8efaa74142f48e4b3fa9ef71b9363206d11
Mut Ze <mutongsoft@gmail.com>
2023-12-18 22:34:43 +0800

    Add /oono command handler

--- oono_akira/modules/_00_oono.py
+++ oono_akira/modules/_00_oono.py
@@ -1,6 +1,29 @@
-from oono_akira.modules import register
+from oono_akira.modules import HandlerType, register
+from oono_akira.slack import SlackContext
 
 
 @register("/oono")
-def null(*_):
-    return
+def handler(context: SlackContext, locked: bool) -> HandlerType:
+    return process, {}
+
+
+async def process(context: SlackContext):
+    await context.ack()
+
+    command = context.must_command()
+    await context.api.chat.postEphemeral(
+        {
+            "channel": command.channel_id,
+            "user": command.user_id,
+            "text": "\n".join(
+                [
+                    "Hello from Oono Akira!",
+                    "",
+                    f"Self: {context.workspace.botId}",
+                    f"Workspace: {command.team_id}",
+                    f"Channel: {command.channel_id}",
+                    f"User: {command.user_id}",
+                ]
+            ),
+        }
+    )
