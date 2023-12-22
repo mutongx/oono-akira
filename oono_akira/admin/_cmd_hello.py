47b0285b517e0cff88363673c7841956664d2b92
Mut Ze <mutongsoft@gmail.com>
2023-12-22 22:13:36 +0800

    Add hello command

---
+++ oono_akira/admin/_cmd_hello.py
@@ -0,0 +1,79 @@
+from argparse import ArgumentParser, Namespace
+
+from oono_akira.slack.context import SlackContext
+from oono_akira.slack.send import SlackPayloadDumper
+from oono_akira.slack.block import Block, RichTextElement, RichTextSpan
+
+
+def desc():
+    return "Say hello to Oono Akira!"
+
+
+def setup(parser: ArgumentParser):
+    pass
+
+
+async def handler(context: SlackContext | None, args: Namespace):
+    if context is None:
+        return
+    command = context.must_command()
+    block = Block(
+        type="rich_text",
+        elements=[
+            RichTextElement(
+                type="rich_text_section",
+                elements=[
+                    RichTextSpan(type="text", text="Hello from Oono Akira!\n"),
+                ],
+            ),
+            RichTextElement(
+                type="rich_text_list",
+                style="bullet",
+                elements=[
+                    RichTextElement(
+                        type="rich_text_section",
+                        elements=[
+                            RichTextSpan(type="text", text="Bot ID: "),
+                            RichTextSpan(type="text", text=context.workspace.botId),
+                        ],
+                    ),
+                    RichTextElement(
+                        type="rich_text_section",
+                        elements=[
+                            RichTextSpan(type="text", text="Workspace ID: "),
+                            RichTextSpan(type="text", text=command.team_id),
+                        ],
+                    ),
+                    RichTextElement(
+                        type="rich_text_section",
+                        elements=[
+                            RichTextSpan(type="text", text="Channel ID: "),
+                            RichTextSpan(type="text", text=command.channel_id),
+                        ],
+                    ),
+                    RichTextElement(
+                        type="rich_text_section",
+                        elements=[
+                            RichTextSpan(type="text", text="User ID: "),
+                            RichTextSpan(type="text", text=command.user_id),
+                        ],
+                    ),
+                    RichTextElement(
+                        type="rich_text_section",
+                        elements=[
+                            RichTextSpan(type="text", text="User is admin: "),
+                            RichTextSpan(type="text", text=str(command.user_id == context.workspace.adminId)),
+                        ],
+                    ),
+                ],
+            ),
+        ],
+    )
+    await context.api.chat.postEphemeral(
+        {
+            "channel": command.channel_id,
+            "user": command.user_id,
+            "text": "Hello from Oono Akira!",
+            "blocks": [SlackPayloadDumper.dump(block)],
+        }
+    )
--- oono_akira/modules/_00_oono.py
+++ oono_akira/modules/_00_oono.py
@@ -1,7 +1,8 @@
+import shlex
+
 from oono_akira.modules import Handler, register
+from oono_akira.admin import run_command
 from oono_akira.slack.context import SlackContext
-from oono_akira.slack.send import SlackPayloadDumper
-from oono_akira.slack.block import Block, RichTextElement, RichTextSpan
 
 
 @register("/oono")
@@ -13,74 +14,4 @@ async def process(context: SlackContext):
     await context.ack()
 
     command = context.must_command()
-    await context.api.chat.postEphemeral(
-        {
-            "channel": command.channel_id,
-            "user": command.user_id,
-            "text": "Hello from Oono Akira!",
-            "blocks": [
-                SlackPayloadDumper.dump(
-                    Block(
-                        type="rich_text",
-                        elements=[
-                            RichTextElement(
-                                type="rich_text_section",
-                                elements=[
-                                    RichTextSpan(type="text", text="Hello from Oono Akira!\n"),
-                                ],
-                            ),
-                            RichTextElement(
-                                type="rich_text_list",
-                                style="bullet",
-                                elements=[
-                                    RichTextElement(
-                                        type="rich_text_section",
-                                        elements=[
-                                            RichTextSpan(type="text", text="Bot ID: "),
-                                            RichTextSpan(type="text", text=context.workspace.botId),
-                                        ],
-                                    ),
-                                    RichTextElement(
-                                        type="rich_text_section",
-                                        elements=[
-                                            RichTextSpan(type="text", text="Workspace ID: "),
-                                            RichTextSpan(type="text", text=command.team_id),
-                                        ],
-                                    ),
-                                    RichTextElement(
-                                        type="rich_text_section",
-                                        elements=[
-                                            RichTextSpan(type="text", text="Channel ID: "),
-                                            RichTextSpan(type="text", text=command.channel_id),
-                                        ],
-                                    ),
-                                    RichTextElement(
-                                        type="rich_text_section",
-                                        elements=[
-                                            RichTextSpan(type="text", text="User ID: "),
-                                            RichTextSpan(type="text", text=command.user_id),
-                                        ],
-                                    ),
-                                    RichTextElement(
-                                        type="rich_text_section",
-                                        elements=[
-                                            RichTextSpan(type="text", text="User is admin: "),
-                                            RichTextSpan(
-                                                type="text", text=str(command.user_id == context.workspace.adminId)
-                                            ),
-                                        ],
-                                    ),
-                                ],
-                            ),
-                            RichTextElement(
-                                type="rich_text_preformatted",
-                                elements=[
-                                    RichTextSpan(type="text", text=f"/oono {command.text}"),
-                                ],
-                            ),
-                        ],
-                    )
-                ),
-            ],
-        }
-    )
+    await run_command(context, command.team_id, command.channel_id, command.user_id, shlex.split(command.text))
